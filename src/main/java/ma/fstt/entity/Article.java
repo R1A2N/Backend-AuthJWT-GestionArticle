1eb0e8ef105d5c1837fbe675d0724c64b6a73327
R1A2N <rania.elmagouri@etu.uae.ac.ma>
2024-01-12 23:48:22 +0100

    ADD-Article

--- src/main/java/ma/fstt/entity/Article.java
+++ src/main/java/ma/fstt/entity/Article.java
@@ -1,4 +1,21 @@
 package ma.fstt.entity;
 
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Table
 public class Article {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String description;
+
+    private Double prix;
+
+    private  String image;
 }

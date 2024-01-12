ea31e6c2565e60bb7bd257795a48f3dd2a330702
R1A2N <rania.elmagouri@etu.uae.ac.ma>
2024-01-12 23:49:12 +0100

    ADD-Category

--- src/main/java/ma/fstt/entity/Categorie.java
+++ src/main/java/ma/fstt/entity/Categorie.java
@@ -1,4 +1,18 @@
 package ma.fstt.entity;
 
+
+import lombok.Data;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Table
 public class Categorie {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private  String image;
 }

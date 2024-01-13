d747d5806795b4213ad35aa4b3170d21b08674ba
R1A2N <rania.elmagouri@etu.uae.ac.ma>
2024-01-13 15:16:30 +0100

    Update

--- src/main/java/ma/fstt/entity/Article.java
+++ src/main/java/ma/fstt/entity/Article.java
@@ -1,21 +1,75 @@
 package ma.fstt.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import lombok.Data;
+import javax.persistence.*;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Data
+@Entity
 @Table
-public class Article {
+public class Article  {
+
+
     @Id
-    @GeneratedValue
     private Integer id;
     private String description;
+    private  Double price;
 
-    private Double prix;
+    public Integer getId() {
+        return id;
+    }
 
-    private  String image;
+    public Article(String description, Double price, String image) {
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+
+    public Article(Integer id, String description, Double price, String image) {
+        this.id = id;
+        this.description = description;
+        this.price = price;
+        this.image = image;
+    }
+
+    public Article() {
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getImage() {
+        return image;
+    }
+    @Lob
+    @Column(name = "image_data", columnDefinition = "BLOB")
+    private byte[] imageData;
+
+
+    private String image;
+    @ManyToOne
+    @JoinColumn(name = "catalogue_id")
+    @JsonBackReference
+    private Categorie catalogue;
+
 }
--- src/main/java/ma/fstt/entity/Categorie.java
+++ src/main/java/ma/fstt/entity/Categorie.java
@@ -1,18 +1,49 @@
 package ma.fstt.entity;
 
 
-import lombok.Data;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.List;
 
-@Data
+@Entity
 @Table
 public class Categorie {
     @Id
     @GeneratedValue
     private Integer id;
+
     private String name;
-    private  String image;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Categorie() {
+    }
+
+    public Categorie(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Categorie(String name) {
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    @OneToMany(mappedBy = "catalogue", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Article> artcles;
+
 }

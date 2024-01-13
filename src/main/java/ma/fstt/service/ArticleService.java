18e8de06744b3c9bd307007cae0843c3a044e605
R1A2N <rania.elmagouri@etu.uae.ac.ma>
2024-01-13 17:03:43 +0100

    add Image-SRVC-CONTROLLER

--- src/main/java/ma/fstt/service/ArticleService.java
+++ src/main/java/ma/fstt/service/ArticleService.java
@@ -1,5 +1,35 @@
 package ma.fstt.service;
 
+import ma.fstt.entity.Article;
+import ma.fstt.entity.Categorie;
+import ma.fstt.repository.Articlerepo;
+import ma.fstt.repository.Categoryrepo;
+
+import javax.annotation.Resource;
+import java.util.List;
+
 public class ArticleService {
+    @Resource
+    private Articlerepo categoryrepo;
+
+    //ajouter catalogue
+    public Article ajouter_catalogue(Article ctlg){
+        return categoryrepo.save(ctlg);
+
+    }
+    //get la list des catalogue
+    public List<Article> getlist(){
+        return  categoryrepo.findAll();
+
+    }
+
+    //delete catalogue
+    public void deletecatal(long id){
+        categoryrepo.deleteById(id);
+
+    }
+    public Article getCatalqueById(Long id) {
+        return categoryrepo.findById(id).orElse(null);
+    }
 
 }
